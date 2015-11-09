22af83c12e2ead343237386157a5a1a14d67d193
h4773rJL <heatseeker.persa@gmail.com>
2015-11-08 22:11:29 -0600

    hilos, actividad 1

--- Hilos-eagregados.java
+++ Hilos-eagregados.java
@@ -25,15 +25,21 @@ public class Hilos extends Thread{ //nombre de la clase
 		// TODO code applicantion logic here
 		Hilos hilo1=new Hilos("Proceso 1"); // Creación del objeto hilos1 de la clase Hilos
 		Hilos hilo2=new Hilos("Proceso 2"); // Creacion del objeto hilos2 de la calse Hilos
+		Hilos hilo3=new Hilos("Proceso 3");
+		Hilos hilo4=new Hilos("Proceso 4");
 		hilo1.setMesaje("Este es el mensaje de proceso 1"); //mensaje para el hilo1
-		hilo2.setMesaje("Mensaje proceso 2"); //Mensaje para el hilo 2		
+		hilo2.setMesaje("Mensaje proceso 2"); //Mensaje para el hilo 2
+		hilo3.setMesaje("Mensaje proceso tercero");
+		hilo4.setMesaje("Mensaje proceso 4");
 		hilo1.start(); //inicio del hilo1 con el metodo start() heredado de la clase Thread
-		//hilo2.setPriority(01); //establezco una prioridad 1
+		hilo2.setPriority(01); //establezco una prioridad 1
 		hilo2.start(); //inicio del hilo2 con el metodo start() heredado de la clase Thread
-		/*try{
-		    hilo1.sleep(1*10*1000); // Duerme durante 1 minuto
+		hilo4.start();
+		hilo3.start();
+		try{
+		    hilo1.sleep(1*60*1000); // Duerme durante 1 minuto
 		    }catch(InterruptedException ex){}
-		  */		   
+		    
 	 }
  }	           
          
